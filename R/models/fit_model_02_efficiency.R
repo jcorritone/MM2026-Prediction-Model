944ed5967fbeaf16a8265824f341161b03f962ec
Joseph Corritone <jcorritone@gmail.com>
2026-03-16 20:03:07 +0100

    Update fit_model_02_efficiency.R

--- R/models/fit_model_02_efficiency.R
+++ R/models/fit_model_02_efficiency.R
@@ -4,14 +4,13 @@ fit_model_02_efficiency <- function(train_data) {
     select(
       Outcome,
       SeedDiff,
-      NetEffDiff,
       OffEffDiff,
       DefEffDiff
     ) %>%
     drop_na()
 
   glm(
-    Outcome ~ SeedDiff + NetEffDiff + OffEffDiff + DefEffDiff,
+    Outcome ~ SeedDiff + OffEffDiff + DefEffDiff,
     data = model_data,
     family = binomial(link = "logit")
   )
