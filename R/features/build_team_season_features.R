1d53b71a705d4722873caa2c9ad8f3a8f4e2eba7
Joseph Corritone <jcorritone@gmail.com>
2026-03-16 19:48:06 +0100

    Update build_team_season_features.R

--- R/features/build_team_season_features.R
+++ R/features/build_team_season_features.R
@@ -3,6 +3,13 @@ library(tidyverse)
 build_team_season_features <- function(team_games) {
 
   team_games %>%
+    mutate(
+      TeamPossessions = TeamFGA - TeamOR + TeamTO + 0.44 * TeamFTA
+      OppPossessions = OppFGA - OppOR + OppTO + 0.44 * OppFTA
+      OffEff = TeamScore / TeamPossessions
+      DefEff = OppScore / OppPossessions
+      NetEff = OffEff - DefEff) %>%
+  
     group_by(Season, TeamID) %>%
     summarise(
       GamesPlayed = n(),
@@ -11,7 +18,11 @@ build_team_season_features <- function(team_games) {
       AvgTeamScore = mean(TeamScore),
       AvgOppScore = mean(OppScore),
       AvgPointDiff = mean(PointDiff),
-      .groups = "drop"
-    ) %>%
+      AvgPossessions = mean(TeamPossessions),
+      OffEff = mean(OffEff),
+      DefEff = mean(DefEff),
+      NetEff = mean(NetEff),
+      .groups = "drop") %>%
+  
     arrange(Season, TeamID)
 }
