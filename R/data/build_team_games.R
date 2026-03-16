d428ac8af04a93ab51dcd942fa695e3bec7362cb
Joseph Corritone <jcorritone@gmail.com>
2026-03-16 19:41:22 +0100

    Update build_team_games.R

--- R/data/build_team_games.R
+++ R/data/build_team_games.R
@@ -13,7 +13,18 @@ build_team_games <- function(reg_season_results) {
       PointDiff = WScore - LScore,
       Win = 1,
       Location = WLoc,
-      NumOT)
+      NumOT,
+      
+      TeamFGA = WFGA,
+      TeamOR = WOR,
+      TeamTO = WTO,
+      TeamFTA = WFTA,
+      OppFGA = LFGA,
+      OppOR = LOR,
+      OppTO = LTO,
+      OppFTA = LFTA)
+
+  FGA - OR + TO + 0.475 * FTA
 
   loser_rows <- reg_season_results %>%
     transmute(
@@ -28,7 +39,16 @@ build_team_games <- function(reg_season_results) {
       Location = case_when(
         WLoc == "H" ~ "A",
         WLoc == "A" ~ "H", TRUE ~ "N"),
-      NumOT)
+      NumOT,
+      
+      TeamFGA = LFGA,
+      TeamOR = LOR,
+      TeamTO = LTO,
+      TeamFTA = LFTA,
+      OppFGA = WFGA,
+      OppOR = WOR,
+      OppTO = WTO,
+      OppFTA = WFTA)
 
   bind_rows(winner_rows, loser_rows) %>%
     arrange(Season, DayNum, TeamID)
