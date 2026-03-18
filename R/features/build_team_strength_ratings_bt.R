73215a94289e489b28b4ed9e040bf12947441781
Joseph Corritone <jcorritone@gmail.com>
2026-03-18 19:47:02 +0100

    Update build_team_strength_ratings_bt.R

--- R/features/build_team_strength_ratings_bt.R
+++ R/features/build_team_strength_ratings_bt.R
@@ -5,60 +5,51 @@ build_team_strength_ratings_bt <- function(reg_season_results) {
 
   bt_ratings <- reg_season_results %>%
     group_split(Season) %>%
-
     map_dfr(function(season_df) {
 
       season <- unique(season_df$Season)
-
       teams <- sort(unique(c(season_df$WTeamID, season_df$LTeamID)))
 
-      team_lookup <- tibble(
-        TeamID = teams,
-        team = factor(teams, levels = teams))
-
-      bt_games <- season_df %>%
+      player1 <- season_df %>%
         transmute(
-          winner = WTeamID,
-          loser = LTeamID,
-          home_advantage = case_when(
+          team = factor(WTeamID, levels = teams),
+          at.home = case_when(
             WLoc == "H" ~ 1,
-            WLoc == "A" ~ -1,
-            TRUE ~ 0),
-          result = 1) %>%
-      
-        left_join(
-          team_lookup %>%
-            rename(winner = TeamID, winner_team = team),
-          by = "winner") %>%
-      
-        left_join(
-          team_lookup %>%
-            rename(loser = TeamID, loser_team = team),
-          by = "loser")
+            WLoc == "A" ~ 0,
+            TRUE ~ 0
+          )
+        )
+
+      player2 <- season_df %>%
+        transmute(
+          team = factor(LTeamID, levels = teams),
+          at.home = case_when(
+            WLoc == "H" ~ 0,
+            WLoc == "A" ~ 1,
+            TRUE ~ 0
+          )
+        )
 
       fit <- BradleyTerry2::BTm(
-        cbind(result, 1 - result),
-        winner_team,
-        loser_team,
-        ~ home_advantage,
-        data = bt_games)
+        outcome = cbind(rep(1, nrow(season_df)), rep(0, nrow(season_df))),
+        player1 = player1,
+        player2 = player2,
+        formula = ~ team + at.home,
+        id = "team"
+      )
 
-      ability_df <- BradleyTerry2::BTabilities(fit) %>%
+      BradleyTerry2::BTabilities(fit) %>%
         as.data.frame() %>%
-      
         rownames_to_column("TeamID") %>%
-      
         as_tibble() %>%
-      
         transmute(
           Season = season,
           TeamID = as.integer(TeamID),
-          BTRating = ability) %>%
-      
+          BTRating = ability
+        ) %>%
         mutate(
-          BTRating = BTRating - mean(BTRating, na.rm = TRUE))
-
-      ability_df
+          BTRating = BTRating - mean(BTRating, na.rm = TRUE)
+        )
     })
 
   return(bt_ratings)
