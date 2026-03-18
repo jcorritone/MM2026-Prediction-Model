d828aa11e8c80b2f41cd590590802647d594467a
Joseph Corritone <jcorritone@gmail.com>
2026-03-18 18:49:04 +0100

    Update fit_model_03_ridge.R

--- R/models/fit_model_03_ridge.R
+++ R/models/fit_model_03_ridge.R
@@ -14,7 +14,7 @@ fit_model_03_ridge <- function(train_data_ridge) {
     drop_na()
 
   glm(
-    Outcome ~ SeedDiff + RidgeRatingDiff + SOSDiff,
+    Outcome ~ SeedDiff + RidgeRatingDiff,
     data = model_data,
     family = binomial(link = "logit"))
 }
