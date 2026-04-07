096a65ed2083fa0475eb20dc00741fce47f290f3
Joseph Corritone <jcorritone@gmail.com>
2026-04-07 05:29:10 +0200

    Update build_matchup_training_data_recency.R

--- R/features/build_matchup_training_data_recency.R
+++ R/features/build_matchup_training_data_recency.R
@@ -8,47 +8,44 @@ build_matchup_training_data_recency <- function(tourney_results, team_season_fea
       Season,
       Team1 = pmin(WTeamID, LTeamID),
       Team2 = pmax(WTeamID, LTeamID),
-      Outcome = if_else(WTeamID < LTeamID, 1, 0)
-    )
+      Outcome = if_else(WTeamID < LTeamID, 1, 0))
 
   team1_features <- team_season_features %>%
     rename_with(
       .fn = ~ paste0("Team1_", .x),
-      .cols = -Season
-    )
+      .cols = -Season)
 
   team2_features <- team_season_features %>%
     rename_with(
       .fn = ~ paste0("Team2_", .x),
-      .cols = -Season
-    )
+      .cols = -Season)
 
   seeds_clean <- seeds %>%
     filter(Season %in% valid_seasons) %>%
+    
     mutate(
-      SeedNum = readr::parse_number(Seed)
-    ) %>%
+      SeedNum = readr::parse_number(Seed)) %>%
+    
     select(Season, TeamID, Seed, SeedNum)
 
   team1_seeds <- seeds_clean %>%
     rename(
       Team1 = TeamID,
       Team1_Seed = Seed,
-      Team1_SeedNum = SeedNum
-    )
+      Team1_SeedNum = SeedNum)
 
   team2_seeds <- seeds_clean %>%
     rename(
       Team2 = TeamID,
       Team2_Seed = Seed,
-      Team2_SeedNum = SeedNum
-    )
+      Team2_SeedNum = SeedNum)
 
   matchup_training_data_recency <- matchup_base %>%
     left_join(team1_features, by = c("Season", "Team1" = "Team1_TeamID")) %>%
     left_join(team2_features, by = c("Season", "Team2" = "Team2_TeamID")) %>%
     left_join(team1_seeds, by = c("Season", "Team1")) %>%
     left_join(team2_seeds, by = c("Season", "Team2")) %>%
+    
     mutate(
       SeedDiff = Team1_SeedNum - Team2_SeedNum,
 
@@ -57,8 +54,8 @@ build_matchup_training_data_recency <- function(tourney_results, team_season_fea
       RecencyNetEffDiff = Team1_RecencyNetEff - Team2_RecencyNetEff,
       RecencySOSDiff = Team1_RecencySOS - Team2_RecencySOS,
       RecencyAdjTempoDiff = Team1_RecencyAdjTempo - Team2_RecencyAdjTempo,
-      TrendNetEffDiff = Team1_TrendNetEff - Team2_TrendNetEff
-    ) %>%
+      TrendNetEffDiff = Team1_TrendNetEff - Team2_TrendNetEff) %>%
+    
     arrange(Season, Team1, Team2)
 
   return(matchup_training_data_recency)
